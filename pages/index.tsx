5aceb41703d35994436015edee35a867db2871bc
Ivan Sharapov <nolarok@gmail.com>
2020-06-02 06:43:58 +0300

    Redirect from /index to /files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useFileUpload, useFileReader } from '@/helpers/hooks'
-import { FileApi } from '@/api/file'
-import Button from '@material-ui/core/Button'
-
+import React from 'react'
+import { useRouter } from 'next/router'
 
 const Home: React.FC = () => {
-  const [rowContent, setRowContent] = useState<string | null>(null)
-  const [file, setFile] = useState<File| null>(null)
-
-  const onFileAdd = useCallback((files: File[]) => {
-    setFile(null)
-    setRowContent(null)
-    setFile(files[0])
-  }, [])
-
-  const {inputFile, handleOpenInputFile, handleAddFile, error} = useFileUpload(onFileAdd)
-  const { readAsBinaryString, readAsText } = useFileReader()
-
-  useEffect(() => {
-    if (!file) {
-      return
-    }
-
-    readAsText(file).then((data: string | ArrayBuffer | null) => {
-      if (typeof data === 'string') {
-        setRowContent(data)
-        console.log({binaryFile: rowContent})
-      }
-    })
-  }, [file])
-
-  useCallback(() => {
-    console.error(error)
-  }, [error])
+  const router = useRouter()
+  router.replace('/files')
 
   return (
     <div>
-      <Button variant="contained">Default</Button>
-      <p>C:\Users\Ivan\WebstormProjects\parserFront\static\temp</p>
-      <input
-        onChange={handleAddFile}
-        type="file"
-        ref={inputFile}
-        style={{display: 'none'}}
-      />
-      <Button
-        onClick={() => { handleOpenInputFile() }}
-      >
-        Выбрать
-      </Button>
 
-      <Button
-        disabled={!rowContent}
-        onClick={() => {
-          file && rowContent && FileApi.create({
-            content: rowContent,
-            filename: file.name
-          })
-        }}
-      >
-        Отправить
-      </Button>
     </div>
   )
 }
